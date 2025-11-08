602dcb915c4daa3c57365111a07985780129206d
MasterKeys <contact.masterkeys@gmail.com>
2025-11-08 22:00:39 +0700

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseAnon = import.meta.env.VITE_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnon);
-
